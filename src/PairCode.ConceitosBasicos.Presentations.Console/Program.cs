e815db41985adaf83b79213b42ee07d1d84c8264
Italo Silveira <italobrian@gmail.com>
2023-06-14 04:42:00 -0300

    primeiro commit conceitos básicos
    solução e variáveis

---
+++ src/PairCode.ConceitosBasicos.Presentations.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Hello, World!");
+var keepRunning = true;
+
+await Task.Delay(1000);
+
+do
+{
+    Console.Clear();
+    Console.Write("Type the command: ");
+    var command = Console.ReadLine();
+
+    switch (command?.ToLower())
+    {
+        case "variables":
+            {
+                Console.WriteLine("Variables\n");
+
+                Console.Write("This is the variable (A) with is a string, type it's value: ");
+                var A = Console.ReadLine();
+
+                Console.WriteLine($"The value typed for (A) is: {A}");
+            }
+            break;
+    }
+
+    Console.Write("\n\nType enter to continue..."); 
+    Console.ReadLine();
+    keepRunning = command != "exit";
+} while (keepRunning);
